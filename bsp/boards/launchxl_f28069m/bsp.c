2721f3a19ff750b34b31ab5011a69a2a7559e4e4
Darren <Darren_Serious.hotmail.com>
2023-12-21 17:21:29 +0800

    feat(boards): add luanchpad f28069m

---
+++ bsp/boards/launchxl_f28069m/bsp.c
@@ -0,0 +1,10 @@
+/*
+ * bsp.c
+ *
+ *  Created on: Dec 17, 2023
+ *      Author: darren
+ */
+
+#include "bsp.h"
+
+#if __cpp
---
+++ bsp/boards/launchxl_f28069m/bsp.h
@@ -0,0 +1,21 @@
+/*
+ * bsp.h
+ *
+ *  Created on: Dec 17, 2023
+ *      Author: darren
+ */
+
+#ifndef __LAUNCHXL_F28069M_BSP_H__
+#define __LAUNCHXL_F28069M_BSP_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif /* extern "C" */
+
+#endif /* __LAUNCHXL_F28069M_BSP_H__ */
